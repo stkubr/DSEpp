13ac501a07f9f795938325b84ec75d2596de3290
stkubr <stkubr@gmail.com>
2014-09-13 17:24:27 +0200

    now take ref to std::function to integrate instead of virtual Integrand()
    Also 2D integration routine was added

--- source/NumLibs/OneLoopIntegrator.hpp
+++ source/NumLibs/OneLoopIntegrator.hpp
@@ -8,45 +8,66 @@
 #ifndef ONELOOPINTEGRATOR_HPP_
 #define ONELOOPINTEGRATOR_HPP_
 
-class C_OneLoopIntegrator{
+#include "../Abs/Kinematics.hpp"
+
+template <typename T_out, typename T_in, typename T_arg> class C_OneLoopIntegrator{
 	public:
-	t_cmplxArray1D integrand_args;
+    T_arg integrand_args;
 	int k_col,Int_counter;
 	C_Kinematics_1loop Momenta;
-	C_Integrator_Line<t_cmplxMatrix,C_OneLoopIntegrator,double> * Integ_radial;
-	C_Integrator_Line<t_cmplxMatrix,C_OneLoopIntegrator,double> * Integ_angle_cheb;
-	C_Integrator_Line<t_cmplxMatrix,C_OneLoopIntegrator,double> * Integ_angle_Y;
+	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integ_radial;
+	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integ_angle_Z;
+	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integ_angle_Y;
+
+    std::function<t_cmplxMatrix(t_cmplxArray1D)>  *integrand;
 
 	bool flag_sigma;
 
 	C_OneLoopIntegrator(){
-		integrand_args.resize(3);
+		//integrand_args.resize(3);
 	}
 
 	//______________________________________________________________________
-	// Multidimensional integration routine
-	t_cmplxMatrix quad3d(){
-		//integrand=func;
+	// 3d-dimensional integration routine
+	T_out MultiDimInt3D(std::function<t_cmplxMatrix(t_cmplxArray1D)>  *func_to_int){
+		integrand=func_to_int;
 		k_col=0;
 		Int_counter=0;
-		return Integ_radial->getResult(&C_OneLoopIntegrator::f1,this);
+		return Integ_radial->getResult(&C_OneLoopIntegrator::f1_3D,this);
 	}
-	t_cmplxMatrix f1(double k){
+	T_out f1_3D(T_in modul_momenta){
 		k_col++;
-		integrand_args[0]=(k);
-		return Integ_angle_cheb->getResult(&C_OneLoopIntegrator::f2,this);
+		integrand_args[0]=(modul_momenta);
+		return Integ_angle_Z->getResult(&C_OneLoopIntegrator::f2_3D,this);
 	}
-	t_cmplxMatrix f2(double z){
-		integrand_args[1]=z;
+	T_out f2_3D(T_in angle_z){
+		integrand_args[1]= angle_z;
 		flag_sigma=true;
-		return Integ_angle_Y->getResult(&C_OneLoopIntegrator::f3,this);
+		return Integ_angle_Y->getResult(&C_OneLoopIntegrator::f3_3D,this);
 	}
-	t_cmplxMatrix f3(double y){
-		integrand_args[2]=y;
-		return (this->Integrand)(integrand_args);
+	T_out f3_3D(T_in angle_y){
+		integrand_args[2]= angle_y;
+		return (*integrand)(integrand_args);
 	}
 
-	virtual t_cmplxMatrix Integrand(t_cmplxArray1D args) {t_cmplxMatrix dummy; std::cout << "Error virtual call" << std::endl; assert(false); return dummy;}
+
+    //______________________________________________________________________
+    // 2d-dimensional integration routine
+    T_out MultiDimInt2D(std::function<t_cmplxMatrix(t_cmplxArray1D)>  *func_to_int){
+        integrand=func_to_int;
+        k_col=0;
+        Int_counter=0;
+        return Integ_radial->getResult(&C_OneLoopIntegrator::f1_2D,this);
+    }
+    T_out f1_2D(T_in modul_momenta){
+        k_col++;
+        integrand_args[0]=(modul_momenta);
+        return Integ_angle_Z->getResult(&C_OneLoopIntegrator::f2_2D,this);
+    }
+    T_out f2_2D(T_in angle_z){
+        integrand_args[1]= angle_z;
+        return (*integrand)(integrand_args);
+    }
 };
 
 
