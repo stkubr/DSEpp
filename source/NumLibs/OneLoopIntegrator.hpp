680aa043bd6573ca9721a0d4673b17db1eeb3d9f
stkubr <stkubr@gmail.com>
2014-09-14 17:02:30 +0200

    Refactoring.

--- source/NumLibs/OneLoopIntegrator.hpp
+++ source/NumLibs/OneLoopIntegrator.hpp
@@ -15,35 +15,32 @@ template <typename T_out, typename T_in, typename T_arg> class C_OneLoopIntegrat
     T_arg integrand_args;
 	int k_col,Int_counter;
 	C_Kinematics_1loop Momenta;
-	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integ_radial;
-	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integ_angle_Z;
-	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integ_angle_Y;
+	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integrator_momentum;
+	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integrator_angle_Z;
+	C_Integrator_Line<T_out,C_OneLoopIntegrator,T_in> * Integrator_angle_Y;
 
-    std::function<t_cmplxMatrix(t_cmplxArray1D)>  *integrand;
-
-	bool flag_sigma;
+    std::function<T_out(t_cmplxArray1D)>  *integrand;
 
 	C_OneLoopIntegrator(){
-		//integrand_args.resize(3);
+		integrand_args.resize(3);
 	}
 
 	//______________________________________________________________________
 	// 3d-dimensional integration routine
-	T_out MultiDimInt3D(std::function<t_cmplxMatrix(t_cmplxArray1D)>  *func_to_int){
+	T_out MultiDimInt3D(std::function<T_out(T_arg)>  *func_to_int){
 		integrand=func_to_int;
 		k_col=0;
 		Int_counter=0;
-		return Integ_radial->getResult(&C_OneLoopIntegrator::f1_3D,this);
+		return Integrator_momentum->getResult(&C_OneLoopIntegrator::f1_3D,this);
 	}
 	T_out f1_3D(T_in modul_momenta){
 		k_col++;
 		integrand_args[0]=(modul_momenta);
-		return Integ_angle_Z->getResult(&C_OneLoopIntegrator::f2_3D,this);
+		return Integrator_angle_Z->getResult(&C_OneLoopIntegrator::f2_3D,this);
 	}
 	T_out f2_3D(T_in angle_z){
 		integrand_args[1]= angle_z;
-		flag_sigma=true;
-		return Integ_angle_Y->getResult(&C_OneLoopIntegrator::f3_3D,this);
+		return Integrator_angle_Y->getResult(&C_OneLoopIntegrator::f3_3D,this);
 	}
 	T_out f3_3D(T_in angle_y){
 		integrand_args[2]= angle_y;
@@ -53,16 +50,16 @@ template <typename T_out, typename T_in, typename T_arg> class C_OneLoopIntegrat
 
     //______________________________________________________________________
     // 2d-dimensional integration routine
-    T_out MultiDimInt2D(std::function<t_cmplxMatrix(t_cmplxArray1D)>  *func_to_int){
+    T_out MultiDimInt2D(std::function<T_out(T_arg)>  *func_to_int){
         integrand=func_to_int;
         k_col=0;
         Int_counter=0;
-        return Integ_radial->getResult(&C_OneLoopIntegrator::f1_2D,this);
+        return Integrator_momentum->getResult(&C_OneLoopIntegrator::f1_2D,this);
     }
     T_out f1_2D(T_in modul_momenta){
         k_col++;
         integrand_args[0]=(modul_momenta);
-        return Integ_angle_Z->getResult(&C_OneLoopIntegrator::f2_2D,this);
+        return Integrator_angle_Z->getResult(&C_OneLoopIntegrator::f2_2D,this);
     }
     T_out f2_2D(T_in angle_z){
         integrand_args[1]= angle_z;
