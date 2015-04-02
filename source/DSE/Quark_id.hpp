d76357bedefcd09c33dab71d2bee7c1296c788bb
stkubr <stkubr@gmail.com>
2015-04-02 12:19:48 +0200

    remove legacy comments

--- source/DSE/Quark_id.hpp
+++ source/DSE/Quark_id.hpp
@@ -73,38 +73,8 @@ public:
 };
 
 
-// Parameterized Factory Method function
-/*C_Quark* C_Quark::createQuark( Quark_ID id ){
-	C_Quark * p;
-	switch (id){
-	case Up_ID:
-		p = new C_Up_Quark();
-		break;
-	case Down_ID:
-		p = new C_Down_Quark();
-		break;
-	case Strange_ID:
-		p = new C_Strange_Quark();
-		break;
-	case Charm_ID:
-		p = new C_Charm_Quark();
-		break;
-	case Test_ID:
-		p = new C_Test_Quark();
-		break;
-	default:
-		assert( false);
-	}
-	return p;
-};*/
-
-
 class C_Quark_Factory: public C_Propagator_Factory{
 public:
-	/*C_Propagator* Create(int _id) {
-		Quark_ID id=(Quark_ID)_id;
-		return C_Quark::createQuark( (id) );
-	}*/
 
 	C_Propagator* Create(int _id) {
 		Quark_ID id = (Quark_ID) _id;
