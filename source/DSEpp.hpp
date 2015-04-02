599c3e48e75f7746a2fe1a780ca75235ed820048
stkubr <stkubr@gmail.com>
2015-04-02 10:35:33 +0200

    add single header file for external use of DSEpp library

---
+++ source/DSEpp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by stkubr on 01.04.15.
+//
+
+#ifndef _DSEPP_DSEPP_HPP_
+#define _DSEPP_DSEPP_HPP_
+
+class C_AbstractKernel;
+class C_Propagator;
+
+#include "../source/types.h"
+#include "../source/DSE/Gluon.hpp"
+#include "../source/Kernel/KernelFactory.hpp"
+#include "../source/DSE/Quark_id.hpp"
+
+
+#endif //_DSEPP_DSEPP_HPP_
