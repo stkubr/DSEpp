f9b73a35d52d4c02fa6293d4b1d87368b4390381
stkubr <stkubr@gmail.com>
2015-04-08 21:59:58 +0200

    change to protected the constructor; add the include "types.h"

--- source/Abs/AbsDiagram.hpp
+++ source/Abs/AbsDiagram.hpp
@@ -1,12 +1,11 @@
 #pragma once
-#include "../DedicMem/DedicMem.hpp"
 
+#include "../types.h"
 
 class C_AbsDiagram{
 	protected:
 	string name;
 	int ID;
-	C_DedicMem_Abs * Memory_abs;
 	DiracGamma Z,Y;
 	DiracGamma5 _Y5;
 	DiracSigma SIG;
@@ -15,14 +14,15 @@ class C_AbsDiagram{
 	double pi;
 	t_cmplx ii;
 	
-	public:
+
 	C_AbsDiagram(){
 		ii=t_cmplx(0.0,1.0);
 		pi=3.14159265358979;
 		I=(1.0/4.0*(Z*Z));
 		Y5=I*_Y5;
 	}
-	
+
+	public:
 	void SetNameID(string _name, int _ID){
 		name=_name;
 		ID=_ID;
