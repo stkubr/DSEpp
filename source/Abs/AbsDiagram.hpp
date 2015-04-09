99375b14ae3d37306ac26faeeeba82341233d36a
stkubr <stkubr@gmail.com>
2015-04-09 14:42:56 +0200

    add comments

--- source/Abs/AbsDiagram.hpp
+++ source/Abs/AbsDiagram.hpp
@@ -1,4 +1,10 @@
-#pragma once
+/*
+ * AbsDiagram.hpp
+ *      Author: stkubr
+ */
+
+#ifndef ABSDIAGRAM_HPP_
+#define ABSDIAGRAM_HPP_
 
 #include "../types.h"
 
@@ -6,12 +12,26 @@ class C_AbsDiagram{
 	protected:
 	string name;
 	int ID;
+
+	/// Dirac gamma matrices \f$ \gamma_\mu \f$.
 	DiracGamma Z,Y;
+
+	/// Dirac gamma matrix \f$ \gamma_5 \f$.
 	DiracGamma5 _Y5;
+
+	/// Dirac sigma matrix \f$ \sigma_{\mu\nu} \f$.
 	DiracSigma SIG;
+
+	/// Euclidean metric tensor \f$ g_{\mu\nu} \f$.
 	MetricTensor g;
-	t_cmplxDirac I,Y5;
+
+	/// Unit matrix (4,4) in Dirac space
+	t_cmplxDirac I;
+
+	t_cmplxDirac Y5;
 	double pi;
+
+	/// Imaginary unit
 	t_cmplx ii;
 	
 
@@ -33,3 +53,5 @@ class C_AbsDiagram{
 		std::cout << "This is object -" <<"  "<< name <<"  "<< "with ID -" <<"  "<< ID  << std::endl;
 	}
 };
+
+#endif /* ABSDIAGRAM_HPP_ */
