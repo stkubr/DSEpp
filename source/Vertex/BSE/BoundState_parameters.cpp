51277cb16558db9918c2bab7cfdf39f82b848266
stkubr <stkubr@gmail.com>
2015-04-21 10:21:13 +0200

    move and rename C_BoundState_parameters

---
+++ source/Vertex/BSE/BoundState_parameters.cpp
@@ -0,0 +1,68 @@
+//
+// Created by stkubr on 21.04.15.
+//
+
+#include "BoundState_parameters.h"
+
+void C_BoundState_parameters::print() {
+    std::cout <<
+    "LimDk" << " - " << LimDk << std::endl <<
+    "LimUk" << " - " << LimUk << std::endl <<
+    "zetta_part" << " - " << zetta_part << std::endl <<
+    "OffShell" << " - " << OffShell << std::endl <<
+
+    "NumRadial" << " - " << NumRadial << std::endl <<
+    "Cheb_order" << " - " << Cheb_order << std::endl <<
+    "NumCheb_nod1" << " - " << NumCheb_nod1 << std::endl <<
+    "NumCheb_nod2" << " - " << NumCheb_nod2 << std::endl <<
+    "NumAngleY" << " - " << NumAngleY << std::endl <<
+
+    "NumRadial_Contour" << " - " << NumRadial_Contour << std::endl <<
+    "NumCheb_Contour" << " - " << NumCheb_Contour << std::endl <<
+    "NumAngleY_Contour" << " - " << NumAngleY_Contour << std::endl;
+}
+
+void C_BoundState_parameters::setParams(std::string &_ParamPath) {
+    std::string line;
+    std::ifstream _ParamList(_ParamPath);
+    if ((_ParamList).is_open()) {
+        while ( (_ParamList).good() ) {
+            (_ParamList) >> line >> LimDk;
+            (_ParamList) >> line >> LimUk;
+            (_ParamList) >> line >> zetta_part;
+            (_ParamList) >> line >> OffShell;
+
+            (_ParamList) >> line >> NumRadial;
+            (_ParamList) >> line >> Cheb_order;
+            (_ParamList) >> line >> NumCheb_nod1;
+            (_ParamList) >> line >> NumCheb_nod2;
+            (_ParamList) >> line >> NumAngleY;
+
+            (_ParamList) >> line >> NumRadial_Contour;
+            (_ParamList) >> line >> NumCheb_Contour;
+            (_ParamList) >> line >> NumAngleY_Contour;
+        }
+    }
+    else {
+        std::cout << "Cant open BoundState parameters file!" << std::endl;
+        assert(false);
+    }
+}
+
+void C_BoundState_parameters::setDefault() {
+    std::cout << "Default BoundState parameters!" << std::endl;
+    LimDk = 0.0001;
+    LimUk = 1000;
+    zetta_part = 0.5;
+    OffShell = 0;
+
+    NumRadial = 24;
+    Cheb_order = 1;
+    NumCheb_nod1 = 6;
+    NumCheb_nod2 = 6;
+    NumAngleY = 6;
+
+    NumRadial_Contour = 48;
+    NumCheb_Contour = 8;
+    NumAngleY_Contour = 8;
+}
---
+++ source/Vertex/BSE/BoundState_parameters.h
@@ -0,0 +1,28 @@
+//
+// Created by stkubr on 21.04.15.
+//
+
+#ifndef DSEPP_BOUNDSTATE_PARAMETERS_H
+#define DSEPP_BOUNDSTATE_PARAMETERS_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <assert.h>
+
+class C_BoundState_parameters {
+public:
+    int NumRadial,NumCheb_nod1,NumCheb_nod2,Cheb_order,NumAngleY;
+    int NumRadial_Contour, NumCheb_Contour, NumAngleY_Contour;
+    double LimUk,LimDk,zetta_part;
+    bool OffShell;
+
+    void print();
+
+    void setParams(std::string& _ParamPath);
+
+    void setDefault();
+};
+
+
+#endif //DSEPP_BOUNDSTATE_PARAMETERS_H
