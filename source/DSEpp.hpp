c3e801bf548dd304fc4b876b6a0eee221089b3ea
stkubr <stkubr@gmail.com>
2015-05-02 00:43:37 +0200

    add BSE_Factory to main library header

--- source/DSEpp.hpp
+++ source/DSEpp.hpp
@@ -13,7 +13,6 @@ class C_Propagator;
 #include "../source/types.h"
 #include "../source/Kernel/KernelFactory.hpp"
 #include "../source/DSE/PropagatorFactory.hpp"
-#include "../source/Vertex/BSE/BSE_Pion.h"
-
+#include "../source/Vertex/BSE/BSE_Factory.h"
 
 #endif //_DSEPP_DSEPP_HPP_
