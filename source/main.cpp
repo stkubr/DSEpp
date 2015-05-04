a04bd7eed1337167a4a743b1d8b25e921ed68959
stkubr <stkubr@gmail.com>
2015-05-05 01:23:25 +0200

    update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <iomanip>
+
 #include "omp.h"
 
 #define NUM_PRECISION 7
@@ -7,14 +8,16 @@
 #define BASIS_TYPE 1
 
 #include "DSEpp.hpp"
+#include "Spectra/Binder.h"
+#include "Spectra/MesonBuilder.h"
 
 /// main()
 int main(int __argc, char *__argv[]) {
-    // Time initialization
-    double tstart, tstop, ttime;
-    tstart = (double) clock() / CLOCKS_PER_SEC;
+  // Time initialization
+  double tstart, tstop, ttime;
+  tstart = (double) clock() / CLOCKS_PER_SEC;
 
-    Quark_ID quark_type = Test_ID;
+  /*  Quark_ID quark_type = Test_ID;
     Gluon_ID gluon_type = Test_Gluon_ID;
     Kernel_ID kernel_type = RL_ID;
 
@@ -36,24 +39,43 @@ int main(int __argc, char *__argv[]) {
     //up_quark->dressPropagator();
 
     std::cout << std::setprecision(16) << up_quark->checkSum() << endl;
-    //61.49288903070202
 
-    std::vector<C_Propagator *> Partons;
-    Partons.push_back(up_quark);
-    Partons.push_back(up_quark);
+    std::vector<C_Propagator *> partons;
+    partons.push_back(up_quark);
+    partons.push_back(up_quark);
 
     C_BSE_Factory BSE_Factory = C_BSE_Factory::instance();
 
     C_BSE *pion = BSE_Factory.Create(PseudoScalar_ID);
-    pion->linkToPartons(Partons);
+    pion->linkToPartons(partons);
     pion->linkToKernel(kernel);
-    t_cmplxVector P;
-    P.SetP4(0, 0, 0, t_cmplx(0, 0.1));
-    //pion.dressBSE(P);
-    pion->calcEigenStates(P, 10);
 
-    tstop = (double) clock() / CLOCKS_PER_SEC;
-    ttime = (tstop - tstart) / omp_get_max_threads();
-    cout << "\n\n" << "calculation time=" << "  " << ttime << "  " << "seconds" << endl;
-    return 0;
+    t_cmplxVector P;
+    P.SetP4(0, 0, 0, t_cmplx(0, 0.138));
+    //pion.dressBSE(P);
+    pion->calcEigenStates(P, 10);*/
+
+  C_Spectra * spectra;
+
+  Binder.SetBSEBuilder(&TestBuilder);
+  Binder.ConstructPhysState();
+  spectra=Binder.GetPhysicalState();
+  spectra->checkAllPieces();
+
+  t_cmplxVector P;
+  P.SetP4(0, 0, 0, t_cmplx(0, 0.138));
+  spectra->BSEs[0]->calcEigenStates(P, 10);
+
+  P.SetP4(0, 0, 0, t_cmplx(0, 0.650));
+  spectra->BSEs[1]->calcEigenStates(P, 10);
+
+  P.SetP4(0, 0, 0, t_cmplx(0, 0.757));
+  spectra->BSEs[2]->calcEigenStates(P, 10);
+
+
+
+  tstop = (double) clock() / CLOCKS_PER_SEC;
+  ttime = (tstop - tstart) / omp_get_max_threads();
+  cout << "\n\n" << "calculation time=" << "  " << ttime << "  " << "seconds" << endl;
+  return 0;
 }
